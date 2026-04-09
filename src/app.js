66a9e26fbd0115d5dda91a6068f580088df00ee5
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2026-04-10 00:23:19 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ app.use(express.json());
 // Routes
 app.use('/', schoolRoutes);
 
+// Health Check Endpoint
+app.use("/health", (req, res) => {
+    res.status(200).json({ status: "OK", message: "Server is healthy" });
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
